710b455e696f2747196af811638217ab9fb998c7
Pazaz <pazaz@itspazaz.com>
2025-01-16 19:58:29 -0500

    feat: No console logging on prod builds, separate scripts

--- webclient/bundle.js
+++ webclient/bundle.js
@@ -1,17 +1,30 @@
 import fs from 'fs';
 
-const build = await Bun.build({
-    entrypoints: ['./src/client/Client.ts'],
-    outdir: './out',
-    sourcemap: 'external',
-    minify: true
-});
+const args = process.argv.slice(2);
 
+function bunBuild(entry, prod) {
+    if (prod) {
+        return Bun.build({
+            entrypoints: [entry],
+            outdir: './out',
+            sourcemap: 'external',
+            minify: true,
+            drop: ['console']
+        });
+    } else {
+        return Bun.build({
+            entrypoints: [entry],
+            outdir: './out'
+        });
+    }
+}
+
+const build = await bunBuild('./src/client/Client.ts', args[0] === 'prod');
 if (!build.success) {
     build.logs.forEach(x => console.log(x));
 } else {
-    fs.copyFileSync('out/Client.js', '../Server/public/js/Client.js');
-    fs.copyFileSync('out/Client.js.map', '../Server/public/js/Client.js.map');
-    fs.copyFileSync('src/3rdparty/bzip2-wasm/bzip2.wasm', '../Server/public/js/bzip2.wasm');
-    fs.copyFileSync('src/3rdparty/tinymidipcm/tinymidipcm.wasm', '../Server/public/js/tinymidipcm.wasm');
+    fs.copyFileSync('out/Client.js', '../Server/public/client/Client.js');
+    fs.copyFileSync('out/Client.js.map', '../Server/public/client/Client.js.map');
+    fs.copyFileSync('src/3rdparty/bzip2-wasm/bzip2.wasm', '../Server/public/client/bzip2.wasm');
+    fs.copyFileSync('src/3rdparty/tinymidipcm/tinymidipcm.wasm', '../Server/public/client/tinymidipcm.wasm');
 }
